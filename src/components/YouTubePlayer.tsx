a0a7cf3f8c4b4943658a357a729a80ee22131617
Wongwc1998 <wongwc1998@gmail.com>
2023-10-01 19:20:40 -0400

    add youtube player component

---
+++ src/components/YouTubePlayer.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import YouTube from "react-youtube";
+
+interface YouTubePlayerProps {
+  videoId: string;
+}
+
+export default function YouTubePlayer({ videoId }: YouTubePlayerProps) {
+  // Set up event handlers
+  const onReady = (event: { target: YT.Player }) => {
+    // Access the player instance
+    const player = event.target;
+
+    // For example, you can automatically play the video
+    player.playVideo();
+  };
+
+  const onError = (error: { data: any; target: YT.Player }) => {
+    console.error("YouTube Player Error:", error);
+  };
+
+  return <YouTube videoId={videoId} onReady={onReady} onError={onError} />;
+}
