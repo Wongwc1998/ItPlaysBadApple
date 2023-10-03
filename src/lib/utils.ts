1eaab32a702bbe15ea366b1e9bdfcca5ba6adecd
Wongwc1998 <wongwc1998@gmail.com>
2023-10-03 16:53:33 -0400

    add utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,106 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { toast } from "sonner"
+import * as z from "zod"
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+export function formatNumber(
+  number: number | string,
+  options: {
+    decimals?: number
+    style?: Intl.NumberFormatOptions["style"]
+    notation?: Intl.NumberFormatOptions["notation"]
+  } = {}
+) {
+  const { decimals = 0, style = "decimal", notation = "standard" } = options
+
+  return new Intl.NumberFormat("en-US", {
+    style,
+    notation,
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals,
+  }).format(Number(number))
+}
+
+export function formatDate(date: Date | string | number) {
+  return new Intl.DateTimeFormat("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  }).format(new Date(date))
+}
+
+export function formatBytes(
+  bytes: number,
+  decimals = 0,
+  sizeType: "accurate" | "normal" = "normal"
+) {
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"]
+  const accurateSizes = ["Bytes", "KiB", "MiB", "GiB", "TiB"]
+  if (bytes === 0) return "0 Byte"
+  const i = Math.floor(Math.log(bytes) / Math.log(1024))
+  return `${(bytes / Math.pow(1024, i)).toFixed(decimals)} ${
+    sizeType === "accurate" ? accurateSizes[i] ?? "Bytest" : sizes[i] ?? "Bytes"
+  }`
+}
+
+export function formatId(id: number) {
+  return `#${id.toString().padStart(4, "0")}`
+}
+
+export function slugify(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/ /g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+}
+
+export function unslugify(str: string) {
+  return str.replace(/-/g, " ")
+}
+
+export function toTitleCase(str: string) {
+  return str.replace(
+    /\w\S*/g,
+    (txt) => txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase()
+  )
+}
+
+export function toSentenceCase(str: string) {
+  return str
+    .replace(/([A-Z])/g, " $1")
+    .replace(/^./, (str) => str.toUpperCase())
+}
+
+export function truncate(str: string, length: number) {
+  return str.length > length ? `${str.substring(0, length)}...` : str
+}
+
+export function isArrayOfFile(files: unknown): files is File[] {
+  const isArray = Array.isArray(files)
+  if (!isArray) return false
+  return files.every((file) => file instanceof File)
+}
+
+export function catchError(err: unknown) {
+  if (err instanceof z.ZodError) {
+    const errors = err.issues.map((issue) => {
+      return issue.message
+    })
+    return toast(errors.join("\n"))
+  } else if (err instanceof Error) {
+    return toast(err.message)
+  } else {
+    return toast("Something went wrong, please try again later.")
+  }
+}
+
+
+export function isMacOs() {
+  if (typeof window === "undefined") return false
+
+  return window.navigator.userAgent.includes("Mac")
 }
