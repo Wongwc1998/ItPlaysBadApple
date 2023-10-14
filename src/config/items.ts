fe1ab1a018951f93d2d5a90804a1274a4e597fe3
Wongwc1998 <wongwc1998@gmail.com>
2023-10-14 11:50:48 -0400

    install more ui compoentns

--- src/config/items.ts
+++ src/config/items.ts
@@ -8,8 +8,6 @@ export const sortOptions = [
     label: "Date: New to old",
     value: "createdAt.desc",
   },
-  { label: "Price: Low to high", value: "price.asc" },
-  { label: "Price: High to low", value: "price.desc" },
   {
     label: "Alphabetical: A to Z",
     value: "name.asc",
