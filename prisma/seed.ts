933a84a922bd97ada71156d36fa049e3109cfaa5
Wongwc1998 <wongwc1998@gmail.com>
2023-10-14 20:14:30 -0400

    make seed script DRYer

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,65 +2,55 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const author1 = await prisma.author.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      name: "Author 1",
-      url: "https://google.com",
-    },
-  });
+  const authorsData = [
+    { id: 1, name: "Author 1", url: "https://google.com" },
+    { id: 2, name: "Author 2", url: "https://google.com" },
+  ];
 
-  const author2 = await prisma.author.upsert({
-    where: { id: 2 },
-    update: {},
-    create: {
-      name: "Author 2",
-      url: "https://google.com",
-    },
-  });
-  const Item1 = await prisma.item.upsert({
-    where: { id: 1 },
-    update: {
-      title:
-        "Bad Apple!! played on Desmos but it's high quality with Bezier Curves",
+  const itemsData = [
+    {
+      id: 1,
+      title: "Bad Apple!! played on Desmos but it's high quality with Bezier Curves",
       description: "This is a sample item description.",
       authorId: 1,
       videoUrl: "MVrNn5TuMkY",
       previewImgUrl: "https://img.youtube.com/vi/MVrNn5TuMkY/hqdefault.jpg",
-      tags: ["software", "bezier-curves"]
+      tags: ["software", "modern", "software-application", "digital"],
     },
-    create: {
-      title:
-        "Bad Apple!! played on Desmos but it's high quality with Bezier Curves",
-      description: "This is a sample item description.",
-      authorId: 1,
-      videoUrl: "MVrNn5TuMkY",
-      previewImgUrl: "https://img.youtube.com/vi/MVrNn5TuMkY/hqdefault.jpg",
-      tags: ["software", "bezier-curves"]
-    },
-  });
-  const Item2 = await prisma.item.upsert({
-    where: { id: 2 },
-    update: {
+    {
+      id: 2,
       title: "Bad Apple!! played on Google Maps",
       description: "Another sample item description.",
       authorId: 2,
       videoUrl: "r-axdVfM0c0",
       previewImgUrl: "https://img.youtube.com/vi/r-axdVfM0c0/hqdefault.jpg",
-      tags: ["software", "google"]
+      tags: ["software", "modern", "software-application", "digital"],
     },
-    create: {
-      title: "Bad Apple!! played on Google Maps",
-      description: "Another sample item description.",
-      authorId: 2,
-      videoUrl: "r-axdVfM0c0",
-      previewImgUrl: "https://img.youtube.com/vi/r-axdVfM0c0/hqdefault.jpg",
-      tags: ["software", "google"]
-    },
-  });
-  console.log({ Item1, Item2 });
+  ];
+
+  const authors = await Promise.all(
+    authorsData.map((author) => 
+      prisma.author.upsert({
+        where: { id: author.id },
+        update: {},
+        create: author,
+      })
+    )
+  );
+
+  const items = await Promise.all(
+    itemsData.map((item) => 
+      prisma.item.upsert({
+        where: { id: item.id },
+        update: item,
+        create: item,
+      })
+    )
+  );
+
+  console.log({ authors, items });
 }
+
 main()
   .then(async () => {
     await prisma.$disconnect();
