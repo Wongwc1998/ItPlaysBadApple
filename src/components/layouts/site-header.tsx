254f2a0f3237b897d0ca60bf92aac7fa643bee99
Wongwc1998 <wongwc1998@gmail.com>
2023-10-15 20:37:53 -0400

    revert nav

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,15 +1,25 @@
-import { Navs } from "@/components/layouts/navs";
 import { ItemsCombobox } from "@/components/items-combobox";
+import { ThemeToggle } from "@/components/layouts/theme-toggle";
+import { siteConfig } from "@/config/site";
+import { MainNav } from "./main-nav";
+import { MobileNav } from "./mobile-nav";
+import { dashboardConfig } from "@/config/dashboard";
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background">
       <div className="container flex h-16 items-center">
-        <Navs />
+        <MainNav items={siteConfig.mainNav} />
+        <MobileNav
+          mainNavItems={siteConfig.mainNav}
+          sidebarNavItems={dashboardConfig.sidebarNav}
+        />
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-2"></nav>
+          <nav className="flex items-center space-x-2">
+            <ItemsCombobox />
+            <ThemeToggle />
+          </nav>
         </div>
-        <ItemsCombobox />
       </div>
     </header>
   );
