096c27c55491c940410570d2460f03147bdf406f
Wongwc1998 <wongwc1998@gmail.com>
2023-10-15 20:23:46 -0400

    add author ui

--- src/app/(lobby)/Item/[ItemId]/page.tsx
+++ src/app/(lobby)/Item/[ItemId]/page.tsx
@@ -2,6 +2,9 @@ import { notFound } from "next/navigation";
 import VideoIframe from "@/components/YoutubeIframe";
 import { prismaDal } from "@/prismaDal";
 import { getTags } from "@/config/items";
+import { ItemCard } from "@/components/cards/item-card";
+import { Shell } from "@/components/shells/shell";
+import Link from "next/link";
 const dal = prismaDal;
 interface ItemPageProps {
   params: {
@@ -11,42 +14,70 @@ interface ItemPageProps {
 
 export default async function ItemPage({ params }: ItemPageProps) {
   const ItemId = Number(params.ItemId);
-  console.log({ params });
   const result = await dal.getItem(ItemId);
-  //log out the item in the response
-  console.log(result);
   if (!result) {
     return notFound();
   }
-  const itemTags = await getTags();
-  console.log({ itemTags });
+  const author = await dal.getAuthor(result.authorId);
+  const otherItems = await dal.getOtherItems(result.authorId, ItemId);
+  const itemTags = getTags();
   const readableTags = result.tags
     ? result.tags.map((slug) => {
         const tagObj = itemTags.find((option) => option.value === slug);
         return tagObj ? tagObj.label : slug; // fallback to slug if title not found
       })
     : [];
-  console.log({ readableTags });
 
   return (
-    <div className="border flex flex-col md:flex-row m-4 p-4 dark:bg-slate-800 bg-gray-100 rounded items-center justify-center">
-      <VideoIframe videoId={result.videoUrl} title={result.title} />
-      <div className="flex flex-col mx-4">
-        <h1 className="text-xl font-bold">{result.title}</h1>
-        <h2 className="text-lg mt-2">by {result.authorId}</h2>
-        <p className="mt-4">{result.description}</p>
-        <div className="mt-5 flex flex-wrap gap-2">
-          {readableTags &&
-            readableTags.map((tagTitle, idx) => (
-              <div
-                key={idx}
-                className="border border-black px-4 py-2 my-2 rounded dark:bg-slate-950 bg-white"
-              >
-                {tagTitle}
-              </div>
-            ))}
+    <Shell>
+      <div className="m-4 p-4">
+        <div className="border flex flex-col md:flex-row dark:bg-slate-800 bg-gray-100 rounded items-center justify-center">
+          <VideoIframe videoId={result.videoUrl} title={result.title} />
+          <div className="flex flex-col mx-4">
+            <h1 className="text-xl font-bold">{result.title}</h1>
+
+            <h2 className="text-lg mt-2">
+              by{" "}
+              <a href={author?.url} className="text-blue-500 hover:underline">
+                {author?.name}
+              </a>
+            </h2>
+            <p className="mt-4">{result.description}</p>
+            <div className="mt-5 flex flex-wrap gap-2">
+              {readableTags?.map((tagTitle) => (
+                <div
+                  key={tagTitle}
+                  className="border border-black px-4 py-2 my-2 rounded dark:bg-slate-950 bg-white"
+                >
+                  {tagTitle}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
+        {/* other items by the same author */}
+        {author && otherItems.length > 0 ? (
+          <div className="overflow-hidden md:pt-6">
+            <h2 className="line-clamp-1 flex-1 text-2xl font-bold">
+              More items from{" "}
+              <a href={author.url} className="text-blue-500 hover:underline">
+                {author.name}
+              </a>
+            </h2>
+            <div className="overflow-x-auto pb-2 pt-6">
+              <div className="flex w-fit gap-4">
+                {otherItems.map((item) => (
+                  <ItemCard
+                    key={item.id}
+                    Item={item}
+                    className="min-w-[260px]"
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        ) : null}
       </div>
-    </div>
+    </Shell>
   );
 }
